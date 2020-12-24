9292a82eeb0f057d59db1144612d12d9fd607000
Daniel Mora <danny@Daniels-MacBook-Pro-2.local>
2020-12-23 21:48:06 -0800

    Add user agent

--- api/controllers/devices.js
+++ api/controllers/devices.js
@@ -1,8 +1,10 @@
 exports.getIpAddress = (req, res, next) => {
     let forwarded = req.headers['x-forwarded-for'];
     let ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+    let agent = req.headers['user-agent'];
 
     res.status(200).json({
-        ipAddress: ip
+        ipAddress: ip,
+        agent: agent
     });
 }
